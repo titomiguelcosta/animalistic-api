3cd4b9f1b8ccbb6954e8e477350fcbb00a326fa4
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-03-30 18:35:20 +0200

    create service for the camera

---
+++ app/services/camera.py
@@ -0,0 +1,34 @@
+from picamera import PiCamera
+from django.core.signals import request_finished
+from django.dispatch import receiver
+
+
+class Camera():
+    camera = None
+
+    @staticmethod
+    def initialize():
+        camera = PiCamera() if PhotoViewSet.camera is None else PhotoViewSet.camera
+        camera.resolution = (1024, 768)
+        camera.saturation = -100
+        camera.sharpness = -100
+        camera.flash_mode = 'torch'
+        camera.image_effect = 'none'
+        camera.rotation = 270
+        camera.color_effects = (128, 128)
+        camera.brightness = 50
+        camera.contrast = 50
+        camera.awb_mode = 'shade'
+        camera.exposure_mode = 'antishake'
+        # give time to calibrate
+        sleep(2)
+
+        Camera.camera = camera
+
+        return Camera.camera
+
+    @receiver(request_finished)
+    def close(sender, **kwargs):
+        if Camera.camera is not None:
+            Camera.camera.close()
+            Camera.camera = None
--- app/views.py
+++ app/views.py
@@ -6,15 +6,13 @@ from app.permissions import HasAuthToken
 from app.models import Photo
 from app.serializers import PhotoSerializer
 from rest_framework.decorators import action
-from picamera import PiCamera
+from app.services.camera import Camera
 from time import sleep
 from django.conf import settings
 import os
 import io
 from datetime import datetime
 import logging
-from django.core.signals import request_finished
-from django.dispatch import receiver
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
@@ -22,14 +20,13 @@ class PhotoViewSet(viewsets.ModelViewSet):
     serializer_class = PhotoSerializer
     permission_classes = [HasAuthToken]
     logger = logging.getLogger('django')
-    camera = None
 
     @action(detail=False, methods=['post'])
     def take(self, request):
         filename = '%s.jpg' % datetime.now().strftime('%Y%m%d%H%M%S%f')
 
-        PhotoViewSet.initialize_camera()
-        PhotoViewSet.camera.capture(os.path.join(
+        Camera.initialize_camera()
+        Camera.camera.capture(os.path.join(
             settings.MEDIA_ROOT, filename),
             quality=100
         )
@@ -43,7 +40,7 @@ class PhotoViewSet(viewsets.ModelViewSet):
     # @see https://github.com/miguelgrinberg/flask-video-streaming
     @action(detail=False, methods=['get'])
     def stream(self, request):
-        PhotoViewSet.initialize_camera()
+        Camera.initialize_camera()
 
         return StreamingHttpResponse(self.gen(), content_type='multipart/x-mixed-replace; boundary=frame')
 
@@ -54,39 +51,9 @@ class PhotoViewSet(viewsets.ModelViewSet):
 
     def frames(self):
         stream = io.BytesIO()
-        for _ in PhotoViewSet.camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+        for _ in Camera.camera.capture_continuous(stream, 'jpeg', use_video_port=True):
             stream.seek(0)
             yield stream.read()
 
             stream.seek(0)
             stream.truncate()
-
-    @receiver(request_finished)
-    def close(sender, **kwargs):
-        logging.getLogger('django').info('closing connection')
-
-        if PhotoViewSet.camera is not None:
-            PhotoViewSet.camera.close()
-            PhotoViewSet.camera = None
-            logging.getLogger('django').info('camera closed')
-
-    @staticmethod
-    def initialize_camera():
-        camera = PiCamera() if PhotoViewSet.camera is None else PhotoViewSet.camera
-        camera.resolution = (1024, 768)
-        camera.saturation = -100
-        camera.sharpness = -100
-        camera.flash_mode = 'torch'
-        camera.image_effect = 'none'
-        camera.rotation = 270
-        camera.color_effects = (128, 128)
-        camera.brightness = 50
-        camera.contrast = 50
-        camera.awb_mode = 'shade'
-        camera.exposure_mode = 'antishake'
-        # give time to calibrate
-        sleep(2)
-
-        PhotoViewSet.camera = camera
-
-        return PhotoViewSet.camera
