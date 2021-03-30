b3a260b311cd7593209029d75a93ade7d821eb2e
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-03-30 11:42:21 +0200

    comment out code for taking photo

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from app.permissions import HasAuthToken
 from app.models import Photo
 from app.serializers import PhotoSerializer
 from rest_framework.decorators import action
-# from picamera import PiCamera
+from picamera import PiCamera
 from time import sleep
 from django.conf import settings
 import os
@@ -21,22 +21,22 @@ class PhotoViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'])
     def take(self, request):
         filename = '%s.jpg' % datetime.now().strftime('%Y%m%d%H%M%S%f')
-        # camera = PiCamera()
-        # camera.resolution = (2592, 1944)
-        # camera.saturation = -100
-        # camera.sharpness = -100
-        # camera.flash_mode = 'torch'
-        # camera.image_effect = 'none'
-        # camera.rotation = 270
-        # camera.color_effects = (128, 128)
-        # camera.brightness = 50
-        # camera.contrast = 50
-        # camera.awb_mode = 'shade'
-        # camera.exposure_mode = 'antishake'
-        # sleep(2)
-        # camera.start_preview()
-        # camera.capture(os.path.join(settings.MEDIA_ROOT, filename))
-        # camera.stop_preview()
+        camera = PiCamera()
+        camera.resolution = (2592, 1944)
+        camera.saturation = -100
+        camera.sharpness = -100
+        camera.flash_mode = 'torch'
+        camera.image_effect = 'none'
+        camera.rotation = 270
+        camera.color_effects = (128, 128)
+        camera.brightness = 50
+        camera.contrast = 50
+        camera.awb_mode = 'shade'
+        camera.exposure_mode = 'antishake'
+        sleep(2)
+        camera.start_preview()
+        camera.capture(os.path.join(settings.MEDIA_ROOT, filename))
+        camera.stop_preview()
 
         photo = Photo()
         photo.name = filename
