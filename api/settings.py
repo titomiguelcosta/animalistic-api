d8b2510c8c19c1e744db34ab241536749aee1086
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-03-31 10:34:38 +0200

    store configuration of apache and supervisor for reference

--- api/settings.py
+++ api/settings.py
@@ -151,7 +151,9 @@ CORS_ALLOW_HEADERS = [
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
